44b246807f8789d069ee7410c85809a886d8299e
Thomas <chiousse.thomas@gmail.com>
2026-09-21 21:10:57 +0200

    feat: add first endpoint in auth controller for tests purposes

---
+++ HelpDesk.Api/Controllers/AuthController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HelpDesk.Api.Controllers;
+
+[Authorize]
+[Route("/api/auth")]
+public class AuthController : ControllerBase
+{
+
+    [Authorize]
+    [HttpGet("me")]
+    public IActionResult Me()
+    {
+        return Ok(new
+        {
+            IsAuthenticated = User.Identity?.IsAuthenticated
+        });
+    }
+}
