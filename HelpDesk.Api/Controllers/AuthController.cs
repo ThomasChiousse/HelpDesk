35d41e97336fd3a360d36613df2718583e6b6146
Thomas <chiousse.thomas@gmail.com>
2026-09-21 21:16:31 +0200

    fix: add forgotten[ApiController] to Auth controller

--- HelpDesk.Api/Controllers/AuthController.cs
+++ HelpDesk.Api/Controllers/AuthController.cs
@@ -3,11 +3,11 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace HelpDesk.Api.Controllers;
 
+[ApiController]
 [Authorize]
 [Route("/api/auth")]
 public class AuthController : ControllerBase
 {
-
     [Authorize]
     [HttpGet("me")]
     public IActionResult Me()
